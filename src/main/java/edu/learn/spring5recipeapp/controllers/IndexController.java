0bee28589a3de336b74417d65a836611a1f4c7f5
tejassheth <sheth.t.j.2012@gmail.com>
2019-10-18 09:03:42 +0530

    created JPA Query methods and print in log using indexController

--- src/main/java/edu/learn/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/edu/learn/spring5recipeapp/controllers/IndexController.java
@@ -1,8 +1,30 @@
 package edu.learn.spring5recipeapp.controllers;
 
+import edu.learn.spring5recipeapp.domain.Category;
+import edu.learn.spring5recipeapp.domain.UnitOfMeasure;
+import edu.learn.spring5recipeapp.repositories.CategoryRepository;
+import edu.learn.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Slf4j
 public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        log.info("Category Id is : "+category.get().getId());
+        log.info("Unit Of Measure Id is : "+unitOfMeasure.get().getId());
+        return "index";
+    }
 }
--- src/main/java/edu/learn/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/edu/learn/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package edu.learn.spring5recipeapp.repositories;
 import edu.learn.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+    public Optional<UnitOfMeasure> findByDescription(String description);
 }

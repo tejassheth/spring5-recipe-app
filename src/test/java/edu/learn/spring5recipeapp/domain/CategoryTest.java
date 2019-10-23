92ea2a62eccd9b7b3a65b0a56ba7a8a00fdcfae0
tejassheth <sheth.t.j.2012@gmail.com>
2019-10-23 23:58:41 +0530

    adding junit test

---
+++ src/test/java/edu/learn/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,29 @@
+package edu.learn.spring5recipeapp.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+    Category category;
+
+    @BeforeEach
+    public void setUp(){
+        category = new Category();
+    }
+    @Test
+    void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue,category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}

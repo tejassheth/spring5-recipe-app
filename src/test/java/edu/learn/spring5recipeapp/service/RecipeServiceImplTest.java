3989149b21a3624f1eb8de02e4ddfa29af82aea1
tejassheth <sheth.t.j.2012@gmail.com>
2019-10-25 00:09:15 +0530

    use mokito to test RecipeService

---
+++ src/test/java/edu/learn/spring5recipeapp/service/RecipeServiceImplTest.java
@@ -0,0 +1,41 @@
+package edu.learn.spring5recipeapp.service;
+
+import edu.learn.spring5recipeapp.domain.Recipe;
+import edu.learn.spring5recipeapp.repositories.RecipeRepository;
+import jdk.javadoc.internal.doclets.toolkit.taglets.SeeTaglet;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        Set  recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1,recipes.size());
+        verify(recipeRepository,times(1)).findAll();
+    }
+}

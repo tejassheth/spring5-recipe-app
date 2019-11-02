1c35f85817be91b84b8fe7b83bc9de41771db682
Tejas Sheth <sheth.t.j.2012@gmail.com>
2019-11-02 17:45:54 +0530

    Add unimplemented indexController test case

--- src/test/java/edu/learn/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/edu/learn/spring5recipeapp/controllers/IndexControllerTest.java
@@ -68,5 +68,28 @@ class IndexControllerTest {
 
     @Test
     void testGetIndexPage() {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+
+        //then
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }

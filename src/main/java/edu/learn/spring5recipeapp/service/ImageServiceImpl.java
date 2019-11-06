6bbc63b1218ce86204f89f5fca02098a695b8eeb
Tejas Sheth <sheth.t.j.2012@gmail.com>
2019-11-07 02:29:58 +0530

    Saved Image to database in byte

--- src/main/java/edu/learn/spring5recipeapp/service/ImageServiceImpl.java
+++ src/main/java/edu/learn/spring5recipeapp/service/ImageServiceImpl.java
@@ -1,13 +1,40 @@
 package edu.learn.spring5recipeapp.service;
 
+import edu.learn.spring5recipeapp.domain.Recipe;
+import edu.learn.spring5recipeapp.repositories.RecipeRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 @Service
 @Slf4j
+@AllArgsConstructor
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
     @Override
-    public void saveImageFile(Long valueOf, MultipartFile file) {
-        log.debug("Received a file  :- "+file.getName());
+    @Transactional
+    public void saveImageFile(Long id, MultipartFile file) {
+
+        try{
+            Recipe recipe = recipeRepository.findById(id).get();
+            Byte[] bytesOfFile = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for(byte b : file.getBytes()){
+                bytesOfFile[i++]=b;
+            }
+            recipe.setImage(bytesOfFile);
+
+            recipeRepository.save(recipe);
+        }catch (IOException e){
+            //todo handle it better
+            log.error("Error :- "+ e.getMessage(),e);
+        }
     }
 }
